b9462826eeb80a8b61b821075e16dfbe1f4ef8b1
Michael Waldo <mbwcogic@yahoo.com>
2020-01-14 20:20:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   def self.artist_count 
     artist_count = {}
-    @@artists.each do |artist|
+    @@artist.each do |artist|
       if artist_count[artist]
         artist_count[artist] +=1
       else 
